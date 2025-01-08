048c09ec60057c12769022ae7cd49f4a419c2ace
grace-manthey <45363240+grace-manthey@users.noreply.github.com>
2025-01-08 11:53:00 -0900

    commit files

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/* script.js */
+
+//jquery code
+$(document).ready(function(){ // begin document.ready block
+
+	//jquery code here
+
+	$(".beforeimagetest").cndkbeforeafter(
+		{
+		  mode: "drag",
+		  theme: "dark",
+		  beforeText: "BEFORE",
+       	  afterText: "AFTER"
+		}
+	);
+
+		// $('.beforeAfter').beforeAfter({
+		// 	movable: true,
+        //     clickMove: true
+		// });
+
+
+	var pymChild = new pym.Child({ polling: 500 });
+    	pymChild.sendHeight();
+
+}); //end document.ready block
